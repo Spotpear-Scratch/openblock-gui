a7d5853457790b00cfc597316cbf114691dd53d8
Lucien Murray-Pitts <lucienmp_antispam@yahoo.com>
2025-09-28 16:31:05 +0900

    Correcting the wiki button to point to our wiki

--- src/components/menu-bar/menu-bar.jsx
+++ src/components/menu-bar/menu-bar.jsx
@@ -352,7 +352,7 @@ class MenuBar extends React.Component {
         window.open('https://community.openblock.cc');
     }
     handleClickOpenWiki () {
-        window.open('https://wiki.openblock.cc');
+        window.open('https://github.com/Spotpear-Scratch/spotpear-scratch.github.io/wiki');
     }
     restoreOptionMessage (deletedItem) {
         switch (deletedItem) {
